b1f099e2dcf3eb43f2aeada5c77c918d96aa5fee
nischal <nischal.bhatt.87@gmail.com>
2022-04-10 23:44:53 +0800

    committing consumer stuff for message service rabbitmq

---
+++ src/main/java/rabbitmq/consumer/TimotiusConsumerApplication.java
@@ -0,0 +1,13 @@
+package rabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimotiusConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimotiusConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rabbitmq/consumer/TimotiusConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package rabbitmq.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimotiusConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
